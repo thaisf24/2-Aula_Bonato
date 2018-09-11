96139032896138fbb6f04e8923c75958fe9e6b2a
Alunos <Alunos@BUTWKSLI7337.animaedu.intranet>
2018-09-11 19:15:17 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/sin/progmulti/atividadessi2018/ActivityC.java
@@ -0,0 +1,17 @@
+package br.usjt.sin.progmulti.atividadessi2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Example Activity to demonstrate the lifecycle callback methods.
+ * Thais Fernandes da silva RA:201310430
+ */
+
+public class ActivityC extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
